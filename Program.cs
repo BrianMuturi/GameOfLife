2e5cb2c0f53d6b573fe78ae9b9959892b7d26924
Brian Muturi <muturibrian18@gmail.com>
2021-03-11 12:11:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    public class LifeSimulation
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Conway's Game of Life!");
+            bool [,] arr = new bool[20, 20];
+            arr[20,20] = false;
+            arr[1, 1] = true;
+            Console.Write(arr);
+
+        }
+    }
+}
